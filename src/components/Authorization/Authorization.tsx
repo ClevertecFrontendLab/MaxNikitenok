fe67bd0b5681e7627c2c0cb63a8521fad93c9923
Max Nikitenok <xxx250790@gmail.com>
2024-02-16 00:27:10 +0300

    feat: authorization and registration forms

--- src/components/Authorization/Authorization.tsx
+++ src/components/Authorization/Authorization.tsx
@@ -4,47 +4,46 @@ import './Authorization.css';
 
 export const Authorization: React.FC = () => {
     const onFinish = (values: any) => {
-      console.log('Received values of form: ', values);
+        console.log('Received values of form: ', values);
     };
-  
+
     return (
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: 'Please input your Username!' }]}
+        <Form
+            name='normal_login'
+            className='login-form authorization-form'
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
         >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Please input your Password!' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Запомнить меня</Checkbox>
-          </Form.Item>
-  
-          <a className="login-form-forgot" href="">
-          Забыли пароль?
-          </a>
-        </Form.Item>
-  
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
+            <Form.Item
+                name='username'
+                rules={[{ required: true, message: 'Please input your Username!' }]}
+            >
+                <Input
+                    prefix={<span className='site-form-item-prefix'>e-mail:</span>}
+                    placeholder='Username'
+                />
+            </Form.Item>
+            <Form.Item
+                name='password'
+                rules={[{ required: true, message: 'Please input your Password!' }]}
+            >
+                <Input type='password' placeholder='Password' />
+            </Form.Item>
+            <Form.Item>
+                <Form.Item name='remember' valuePropName='checked' noStyle>
+                    <Checkbox>Запомнить меня</Checkbox>
+                </Form.Item>
+
+                <a className='login-form-forgot' href=''>
+                    Забыли пароль?
+                </a>
+            </Form.Item>
+
+            <Form.Item>
+                <Button type='primary' htmlType='submit' className='login-form-button'>
+                    Войти
+                </Button>
+            </Form.Item>
+        </Form>
     );
-  };
+};
--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -4,57 +4,48 @@ import './Registration.css';
 
 export const Registration: React.FC = () => {
     const onFinish = (values: any) => {
-      console.log('Received values of form: ', values);
+        console.log('Received values of form: ', values);
     };
-  
+
     return (
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: 'Please input your Username!' }]}
+        <Form
+            name='normal_login'
+            className='login-form registration-form'
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
         >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Please input your Password!' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item
-          name="passwordRep"
-          rules={[{ required: true, message: 'Please input your Password!' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="passwordRep"
-            placeholder="PasswordRep"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Запомнить меня</Checkbox>
-          </Form.Item>
-  
-          <a className="login-form-forgot" href="">
-          Забыли пароль?
-          </a>
-        </Form.Item>
-  
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
+            <Form.Item
+                name='username'
+                rules={[{ required: true, message: 'Please input your Username!' }]}
+            >
+                <Input
+                    prefix={<span className='site-form-item-prefix'>e-mail:</span>}
+                    placeholder='Username'
+                />
+            </Form.Item>
+            <Form.Item
+                name='password'
+                rules={[{ required: true, message: 'Please input your Password!' }]}
+            >
+                <Input
+                    type='password'
+                    placeholder='Password'
+                />
+            </Form.Item>
+            <Form.Item
+                name='passwordRep'
+                rules={[{ required: true, message: 'Please input your Password!' }]}
+            >
+                <Input
+                    type='passwordRep'
+                    placeholder='PasswordRep'
+                />
+            </Form.Item>
+            <Form.Item>
+                <Button type='primary' htmlType='submit' className='login-form-button'>
+                    Войти
+                </Button>
+            </Form.Item>
+        </Form>
     );
-  };
+};
